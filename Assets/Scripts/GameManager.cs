75a179955a56642d66e1cf48abb4fbaecbc64b14
Remn <egorsliesarenko@gmail.com>
2024-01-30 16:41:20 +0200

    Made a new game mode and fixed the problem with android saying "this app was made for an older version of android"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,26 +8,49 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject popThingy;
     public int lives = 3;
     public Canvas menuCanvas;
+    public int currentGameMode;
 
     void Start()
     {
         Application.targetFrameRate = 60;
     }
-    
+
     public void SpawnPopstacles()
     {
-        Instantiate(popThingy, new Vector3(Random.Range(-2.25f, 2.25f), Random.Range(3f, 4.5f)), Quaternion.Euler(0f, 0f, 0f));
+        switch (currentGameMode)
+        {
+            case 1:
+                {
+                    Instantiate(popThingy, new Vector3(Random.Range(-2.25f, 2.25f), Random.Range(3f, 4.5f)), Quaternion.Euler(0f, 0f, 0f));
+                    break;
+                }
+            case 2:
+                {
+                    Instantiate(popThingy, new Vector3(Random.Range(-2.25f, 2.25f), Random.Range(-4.5f, 4.5f)), Quaternion.Euler(0, 0, Random.Range(-180f, 180f)));
+                    break;
+                }
+        }
     }
 
     public void FallingPopstacles()
     {
+        currentGameMode = 1;
         lives = 3;
         menuCanvas.gameObject.SetActive(false);
         SpawnPopstacles();
     }
-    
+
+    public void StandingPopstackles()
+    {
+        currentGameMode = 2;
+        lives = 3;
+        menuCanvas.gameObject.SetActive(false);
+        SpawnPopstacles();
+    }
+
     public void GameLost()
     {
+        currentGameMode = 0;
         menuCanvas.gameObject.SetActive(true);
     }
 
--- Assets/Scripts/ScriptPopThingy.cs
+++ Assets/Scripts/ScriptPopThingy.cs
@@ -14,25 +14,66 @@ public class ScriptPopThingy : MonoBehaviour, IPointerDownHandler
     {
         gameManager = GameObject.FindGameObjectWithTag("GameController");
         gameManagerScript = gameManager.GetComponent<GameManager>();
+        switch (gameManagerScript.currentGameMode)
+        {
+            case 1:
+                {
+                    break;
+                }
+            case 2:
+                {
+                    gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+                    Invoke("DestroySelf", 1f);
+                    break;
+                }
+        }
     }
 
     private void OnBecameInvisible()
     {
-        if (isDestroyed != true && gameManagerScript.lives > 0)
+        switch (gameManagerScript.currentGameMode)
         {
-            gameManagerScript.lives--;
-            Debug.Log("Lives: " + gameManagerScript.lives);
-            gameManagerScript.SpawnPopstacles();
-            Destroy(gameObject);
-        }
-        else if (gameManagerScript.lives <= 0 && isDestroyed != true)
-        {
-            Debug.Log("Invisible");
-            gameManagerScript.GameLost();
-            Destroy(gameObject);
-        } else
-        {
-            gameManagerScript.SpawnPopstacles();
+            case 1:
+                {
+                    if (isDestroyed != true && gameManagerScript.lives > 0)
+                    {
+                        gameManagerScript.lives--;
+                        Debug.Log("Lives: " + gameManagerScript.lives);
+                        gameManagerScript.SpawnPopstacles();
+                        Destroy(gameObject);
+                    }
+                    else if (gameManagerScript.lives <= 0 && isDestroyed != true)
+                    {
+                        Debug.Log("Invisible");
+                        gameManagerScript.GameLost();
+                        Destroy(gameObject);
+                    }
+                    else
+                    {
+                        gameManagerScript.SpawnPopstacles();
+                    }
+                    break;
+                }
+            case 2:
+                {
+                    if (isDestroyed != true)
+                    {
+                        if (gameManagerScript.lives > 0)
+                        {
+                            gameManagerScript.lives--;
+                            gameManagerScript.SpawnPopstacles();
+                        }
+                        else
+                        {
+                            gameManagerScript.GameLost();
+                        }
+                    }
+                    else
+                    {
+                        gameManagerScript.SpawnPopstacles();
+                    }
+                    break;
+                }
         }
     }
 
@@ -41,4 +82,11 @@ public class ScriptPopThingy : MonoBehaviour, IPointerDownHandler
         isDestroyed = true;
         Destroy(gameObject);
     }
+
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+
+
 }
